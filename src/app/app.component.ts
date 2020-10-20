74f17add5eff917dfccd12d75d404f6a18ead11f
hchockarprasad <hchockarprasad@gmail.com>
2020-10-20 19:10:28 +0530

    Add scroller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,9 @@ import {
 import { WasmService } from './wasm.service';
 
 const containerWidth = 560;
-const scrollerWidth = 3960;
-const containerHeight = 400;
-const scrollerHeight = 10000;
+const scrollerWidth = 39960;
+const containerHeight = 380;
+const scrollerHeight = 1999980;
 const cellWidth = 100;
 const cellHeight = 20;
 
@@ -26,6 +26,8 @@ export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('placeholder') placeholder: ElementRef<HTMLInputElement>;
   @ViewChild('scroller') scroller: ElementRef<HTMLInputElement>;
   @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild('hScrollContainer') hScroller: ElementRef<HTMLElement>;
+  @ViewChild('vScrollContainer') vScroller: ElementRef<HTMLElement>;
   @ViewChild('scrollerContainer') scrollerContainer: ElementRef<HTMLInputElement>;
   constructor(private wasmService: WasmService, private cdr: ChangeDetectorRef) {}
 
@@ -55,6 +57,13 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   moveDown() {
+    let phDiff = this.placeholder.nativeElement.offsetTop - this.scrollerContainer.nativeElement.scrollTop;
+    if (phDiff < 0) {
+      this.scrollerContainer.nativeElement.scrollTop += phDiff;
+      this.vScroller.nativeElement.scrollTop = this.scrollerContainer.nativeElement.scrollTop;
+      this.onScrolled();
+      return;
+    }
     let jumpValue = 20;
     let phTop = this.placeholder.nativeElement.offsetTop;
     if (phTop <= scrollerHeight - jumpValue - cellHeight) {
@@ -64,15 +73,28 @@ export class AppComponent implements OnInit, AfterViewInit {
       let scrollerTop = this.scrollerContainer.nativeElement.scrollTop;
       let scrollerExtent = scrollerTop + containerHeight;
 
-      let diff = placeHolderExtent - scrollerExtent + 20;
+      let diff = placeHolderExtent - scrollerExtent;
       if (diff > 0) {
         this.scrollerContainer.nativeElement.scrollTop += diff;
+        this.vScroller.nativeElement.scrollTop = this.scrollerContainer.nativeElement.scrollTop;
         this.onScrolled();
       }
     }
   }
 
   moveRight() {
+    let phDiff = this.placeholder.nativeElement.offsetLeft - this.scrollerContainer.nativeElement.scrollLeft;
+    if (phDiff < 0) {
+      this.scrollerContainer.nativeElement.scrollLeft += phDiff;
+      this.hScroller.nativeElement.scrollLeft = this.scrollerContainer.nativeElement.scrollLeft;
+      this.onScrolled();
+      return;
+    }
+    if (this.scrollerContainer.nativeElement.scrollLeft !== this.hScroller.nativeElement.scrollLeft) {
+      this.hScroller.nativeElement.scrollLeft = this.scrollerContainer.nativeElement.scrollLeft;
+      this.onScrolled();
+      return;
+    }
     let jumpValue = 100;
     let phleft = this.placeholder.nativeElement.offsetLeft;
     if (phleft <= scrollerWidth - jumpValue - cellWidth) {
@@ -83,15 +105,21 @@ export class AppComponent implements OnInit, AfterViewInit {
 
       let diff = placeHolderExtent - scrollerExtent;
       if (diff > 0) {
-        console.log('gg');
         this.scrollerContainer.nativeElement.scrollLeft += diff;
-        console.log(this.scrollerContainer.nativeElement.scrollLeft);
+        this.hScroller.nativeElement.scrollLeft = this.scrollerContainer.nativeElement.scrollLeft;
         this.onScrolled();
       }
     }
   }
 
   moveLeft() {
+    let phDiff = this.placeholder.nativeElement.offsetLeft - this.scrollerContainer.nativeElement.scrollLeft;
+    if (phDiff < 0) {
+      this.scrollerContainer.nativeElement.scrollLeft += phDiff;
+      this.hScroller.nativeElement.scrollLeft = this.scrollerContainer.nativeElement.scrollLeft;
+      this.onScrolled();
+      return;
+    }
     let jumpValue = 100;
     let phleft = this.placeholder.nativeElement.offsetLeft;
     let res = phleft - jumpValue;
@@ -104,12 +132,20 @@ export class AppComponent implements OnInit, AfterViewInit {
       let diff = placeholderLeft - scrollerleft;
       if (diff < 0) {
         this.scrollerContainer.nativeElement.scrollLeft += diff;
+        this.hScroller.nativeElement.scrollLeft = this.scrollerContainer.nativeElement.scrollLeft;
         this.onScrolled();
       }
     }
   }
 
   moveTop() {
+    let phDiff = this.placeholder.nativeElement.offsetTop - this.scrollerContainer.nativeElement.scrollTop;
+    if (phDiff < 0) {
+      this.scrollerContainer.nativeElement.scrollTop += phDiff;
+      this.vScroller.nativeElement.scrollTop = this.scrollerContainer.nativeElement.scrollTop;
+      this.onScrolled();
+      return;
+    }
     let jumpValue = 20;
     let phtop = this.placeholder.nativeElement.offsetTop;
     let res = phtop - jumpValue;
@@ -122,16 +158,27 @@ export class AppComponent implements OnInit, AfterViewInit {
       let diff = placeholderTop - scrollTop;
       if (diff < 0) {
         this.scrollerContainer.nativeElement.scrollTop += diff;
+        this.vScroller.nativeElement.scrollTop = this.scrollerContainer.nativeElement.scrollTop;
         this.onScrolled();
       }
     }
   }
 
+  onHScroll() {
+    this.scrollerContainer.nativeElement.scrollLeft = this.hScroller.nativeElement.scrollLeft;
+    this.onScrolled();
+  }
+
+  onVScroll() {
+    this.scrollerContainer.nativeElement.scrollTop = this.vScroller.nativeElement.scrollTop;
+    this.onScrolled();
+  }
+
   onScrolled() {
     const scrollLeft = this.scrollerContainer.nativeElement.scrollLeft;
     const scrollTop = this.scrollerContainer.nativeElement.scrollTop;
-    const row = Math.ceil(scrollTop / cellHeight);
-    const col = Math.ceil(scrollLeft / cellWidth);
+    const row = Math.floor(scrollTop / cellHeight);
+    const col = Math.floor(scrollLeft / cellWidth);
     const totalRowSize = row * cellHeight;
     const totalColSize = col * cellWidth;
     const firstRow = {
@@ -142,15 +189,14 @@ export class AppComponent implements OnInit, AfterViewInit {
       idx: col,
       offset: totalColSize - scrollLeft,
     };
-    // console.log(firstRow);
     this.draw(firstRow, firstCol);
-    // console.log('Scroll Left is' + scrollLeft);
   }
 
   draw(firstRow, firstCol) {
     let ctx = this.canvas.nativeElement.getContext('2d');
-    ctx.clearRect(0, 0, containerWidth + 40, containerHeight);
+    ctx.clearRect(0, 0, containerWidth + 40, containerHeight + 20);
     let rowOffset = firstRow.offset;
+    ctx.fillStyle = '#000000';
     let colOffset = firstCol.offset + 40;
     let rowidx = firstRow.idx + 1;
     while (rowOffset <= containerHeight) {
@@ -169,20 +215,23 @@ export class AppComponent implements OnInit, AfterViewInit {
     }
 
     let colidx = firstCol.idx + 1;
-    console.log(colidx);
     while (colOffset <= containerWidth + 40) {
       ctx.beginPath();
       ctx.rect(colOffset, 0, 100, 20);
       ctx.stroke();
-      ctx.fillText(colidx, colOffset + 20 + 15, 15);
+      ctx.fillText(colidx, colOffset + 20 + 20, 15);
       ctx.beginPath();
       ctx.lineWidth = 0.3;
       ctx.strokeStyle = '#000000';
       ctx.moveTo(colOffset, 20);
-      ctx.lineTo(colOffset, containerHeight);
+      ctx.lineTo(colOffset, containerHeight + 20);
       ctx.stroke();
       colidx += 1;
       colOffset += cellWidth;
     }
+
+    ctx.beginPath();
+    ctx.fillStyle = '#dbdbdb';
+    ctx.fillRect(0, 0, 40, 20);
   }
 }
